a1591214fee9c3078cb4d045c70c23bd4a4b8441
rkalesalad <rkaelle2@gmail.com>
2024-07-23 00:18:57 -0700

    testing different key

--- today.py
+++ today.py
@@ -423,7 +423,7 @@ def formatter(query_type, difference, funct_return=False, whitespace=0):
 
 if __name__ == '__main__':
     """
-    Andrew Grant (Andrew6rant), 2022-2023
+    Ryan Kaelle (rkaelle), 2022-2023
     """
     print('Calculation times:')
     # define global variable for owner ID and calculate user's creation date
@@ -431,7 +431,7 @@ if __name__ == '__main__':
     user_data, user_time = perf_counter(user_getter, USER_NAME)
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
-    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2002, 7, 5))
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2005, 4, 20))
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
